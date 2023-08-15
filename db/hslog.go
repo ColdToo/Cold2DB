8b9f5a968c54c5987cf4e2643e94935f78f2e296
hlhf <1095824838@qq.com>
2023-08-15 14:43:06 +0800

    raft hard state log

--- db/hslog.go
+++ db/hslog.go
@@ -1,10 +1,8 @@
 package db
 
 import (
-	"fmt"
 	"github.com/ColdToo/Cold2DB/db/logfile"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -32,63 +30,11 @@ func initHardStateLog(hardStateLogCfg HardStateLogConfig) (hsLog *hardStateLog, 
 }
 
 func (hs *hardStateLog) Read(fid uint32, offset int64) (*logfile.LogEntry, error) {
-	var logFile *logfile.LogFile
-	if fid == hs.activeLogFile.Fid {
-		logFile = hs.activeLogFile
-	} else {
-		hs.RLock()
-		logFile = hs.logFiles[fid]
-		if logFile != nil && logFile.IoSelector == nil {
-			opt := hs.opt
-			lf, err := logfile.OpenLogFile(opt.path, fid, opt.blockSize, logfile.ValueLog, opt.ioType)
-			if err != nil {
-				hs.RUnlock()
-				return nil, err
-			}
-			hs.logFiles[fid] = lf
-			logFile = lf
-		}
-		hs.RUnlock()
-	}
-	if logFile == nil {
-		return nil, fmt.Errorf(ErrLogFileNil.Error(), fid)
-	}
-
-	entry, _, err := logFile.ReadLogEntry(offset)
-	if err == logfile.ErrEndOfEntry {
-		return &logfile.LogEntry{}, nil
-	}
-	return entry, err
+	return nil, nil
 }
 
 func (hs *hardStateLog) Write(ent *logfile.LogEntry) (*valuePos, int, error) {
-	hs.Lock()
-	defer hs.Unlock()
-	buf, eSize := logfile.EncodeEntry(ent)
-	// if active is reach to thereshold, close it and open a new one.
-	if hs.activeLogFile.WriteAt+int64(eSize) >= hs.opt.blockSize {
-		if err := hs.Sync(); err != nil {
-			return nil, 0, err
-		}
-		hs.logFiles[hs.activeLogFile.Fid] = hs.activeLogFile
-
-		logFile, err := hs.createLogFile()
-		if err != nil {
-			return nil, 0, err
-		}
-		hs.activeLogFile = logFile
-	}
-
-	err := hs.activeLogFile.Write(buf)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	writeAt := atomic.LoadInt64(&hs.activeLogFile.WriteAt)
-	return &valuePos{
-		Fid:    hs.activeLogFile.Fid,
-		Offset: writeAt - int64(eSize),
-	}, eSize, nil
+	return nil, 0, nil
 }
 
 func (hs *hardStateLog) Close() error {
