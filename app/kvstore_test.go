629c6ff056ccc92fc03b18b169b9ea3c5cdfba3f
hlhf <1095824838@qq.com>
2023-08-31 20:22:17 +0800

    raft kv prpose 单元测试

--- app/kvstore_test.go
+++ app/kvstore_test.go
@@ -14,89 +14,62 @@ import (
 
 func TestKvStore_Propose_OK(t *testing.T) {
 	initLog()
-	tests := []struct {
-		name string
-		want bool
-	}{
-		{
-			name: "ok",
-			want: true,
-		},
+
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	DB := mock.NewMockDB(mockCtl)
+	gomonkey.ApplyFunc(db.GetDB, func() (db.DB, error) {
+		return DB, nil
+	})
+	time.Sleep(time.Second)
+
+	proposeC := make(chan []byte, 100)
+	kvStore := NewKVStore(proposeC, 5)
+	key := []byte("testKey")
+	val := []byte("testValue")
+	delete := true
+	expiredAt := time.Now().UnixNano()
+
+	go func() {
+		result, _ := kvStore.Propose(key, val, delete, expiredAt)
+		t.Log(result)
+		assert.Equal(t, true, result)
+	}()
+
+	//等待协程处理
+	time.Sleep(time.Second)
+	for k, v := range kvStore.monitorKV {
+		t.Log(k)
+		close(v)
 	}
+	time.Sleep(3 * time.Second)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			mockCtl := gomock.NewController(t)
-			defer mockCtl.Finish()
-			DB := mock.NewMockDB(mockCtl)
-			gomonkey.ApplyFunc(db.GetDB, func() (db.DB, error) {
-				return DB, nil
-			})
-			time.Sleep(time.Second)
-
-			proposeC := make(chan []byte, 100)
-			kvStore := NewKVStore(proposeC, 5)
-			kvStore.ReqTimeout = time.Second
-			key := []byte("testKey")
-			val := []byte("testValue")
-			delete := true
-			expiredAt := time.Now().UnixNano()
-
-			if tt.name == "ok" {
-				go func() {
-					result, _ := kvStore.Propose(key, val, delete, expiredAt)
-					t.Log(result)
-					assert.Equal(t, true, result)
-				}()
-
-				//等待协程给map赋值
-				time.Sleep(time.Second)
-				for _, v := range kvStore.monitorKV {
-					close(v)
-				}
-				time.Sleep(3 * time.Second)
-			}
-		})
-	}
 }
 
 func TestKvStore_Propose_TimeOut(t *testing.T) {
 	initLog()
-	tests := []struct {
-		name string
-		want bool
-	}{
-		{
-			name: "timeout",
-			want: false,
-		},
-	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			mockCtl := gomock.NewController(t)
-			defer mockCtl.Finish()
-			DB := mock.NewMockDB(mockCtl)
-			gomonkey.ApplyFunc(db.GetDB, func() (db.DB, error) {
-				return DB, nil
-			})
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	DB := mock.NewMockDB(mockCtl)
+	gomonkey.ApplyFunc(db.GetDB, func() (db.DB, error) {
+		return DB, nil
+	})
 
-			proposeC := make(chan []byte, 100)
-			kvStore := NewKVStore(proposeC, 5)
-			kvStore.ReqTimeout = time.Second
-			key := []byte("testKey")
-			val := []byte("testValue")
-			delete := true
-			expiredAt := time.Now().UnixNano()
+	proposeC := make(chan []byte, 100)
+	kvStore := NewKVStore(proposeC, 5)
+	kvStore.ReqTimeout = time.Second
+	key := []byte("testKey")
+	val := []byte("testValue")
+	delete := true
+	expiredAt := time.Now().UnixNano()
 
-			flagC := make(chan bool)
-			go func() {
-				result, _ := kvStore.Propose(key, val, delete, expiredAt)
-				t.Log(result)
-				assert.Equal(t, false, result)
-				flagC <- result
-			}()
-			time.Sleep(2 * time.Second)
-		})
-	}
+	flagC := make(chan bool)
+	go func() {
+		result, _ := kvStore.Propose(key, val, delete, expiredAt)
+		t.Log(result)
+		assert.Equal(t, false, result)
+		flagC <- result
+	}()
+	time.Sleep(2 * time.Second)
 }
